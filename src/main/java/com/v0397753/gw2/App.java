9ca13fc57c56f7d71a5dcb776da38ad881990046
Shane Keel <shane.keel@gmail.com>
2026-02-03 14:05:20 -0500

    Initialize Maven project structure

---
+++ src/main/java/com/v0397753/gw2/App.java
@@ -0,0 +1,13 @@
+package com.v0397753.gw2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
